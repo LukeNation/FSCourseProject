0f7f45566f31b6343922c633330903513bf1342b
agustin <agusasaad1099@hotmail.com>
2022-03-06 21:11:32 -0300

    Home/Carpetas

---
+++ ProjectFront/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link to="/" className="navbar-brand" href="#">PANA</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link active" aria-current="page" href="#">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Caracteristicas</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link" href="#">Empezar</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <ul className="navbar-nav"  id="navbarNav">
+                        <li className="nav-item">
+                            <Link to="/login" className="nav-link active" aria-current="page" href="#">Iniciar Sesion</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/register" className="nav-link" href="#">Registrarse</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+export default Navbar;
