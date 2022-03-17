99964ca86adf44050708b18ba1dedd89b2549f7a
agustin <agusasaad1099@hotmail.com>
2022-03-17 15:15:32 -0300

    login

--- ProjectFront/src/components/Home/Header.jsx
+++ ProjectFront/src/components/Home/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
@@ -8,7 +9,7 @@ const Header = () => {
                 <div className='col-md-6 title'>
                     <h1>ORGANIZA TUS PROYECTOS Y TAREAS</h1>
                     <p>PANA es una herramienta visual que te ayuda a gestionar cualquier tipo de proyecto. Los tableros y las listas te ayudan a tener las tareas ordenadas y se pueden personalizar según las necesidades del proyecto. Regístrate ahora, es gratis.</p>
-                    <button id='botonInicio' type='button' className='btn btn-dark'>Empezar ahora</button>
+                    <Link to='/Register'><button id='botonInicio' type='button' className='btn btn-dark'>Empezar ahora</button></Link>
                 </div>
             </div>
         </>
