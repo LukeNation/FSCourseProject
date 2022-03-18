fde6381897ab817fcdf49048238aa832c578bf83
Paula <91703336+PaulaBecariaC@users.noreply.github.com>
2022-03-17 21:49:21 -0300

    PaginaInicio

---
+++ ProjectFront/src/components/SignedInUser/SignedIn.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar2 from '../Navbar2';
+
+
+
+const SignedInUser = () => {
+    return (
+        <div>
+            <Navbar2 />
+
+
+            <div id="board" className='container-fluid'>
+
+
+
+                <div id="board1">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SignedInUser;
