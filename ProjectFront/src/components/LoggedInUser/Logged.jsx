2b35aaca65a8d4fdf062e96b7d2ca89672a1a7e0
agustin <agusasaad1099@hotmail.com>
2022-03-26 13:06:42 -0300

    logged y login modificados

--- ProjectFront/src/components/LoggedInUser/Logged.jsx
+++ ProjectFront/src/components/LoggedInUser/Logged.jsx
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar2 from '../Navbar2';
 import Feature_3 from '../Home/img/Feature_3.webp'; 
 
+
 const Logged = () => {
+
+    const [name, setName] = useState ('Agustin');
+    const [table, setTable] = useState (['table 1', 'table 2', 'table 3']);
+
+    useEffect(()=> {
+    const getName = () => {
+        fetch('http://localhost:4000/User/:id')
+        .then(res => res.json())
+        .then(res => setName(res))
+    }    
+    const getTable = () => {
+        fetch('http://localhost:4000/tables')
+        .then(res => res.json())
+        .then(res => setTable(res))
+    }
+    getName();
+    getTable();
+    }, [])
+
     return (
         <div>
             <Navbar2 />
-
-            <div id="logged" className='container-fluid  text-center'>
-                <div id="loggedMenu" className='row'>
-                    <div id="loggedContent" className='container'>
-                        <div id="textContent">
-                            <h1>¡Hola, nombre usuario!</h1>
-                            <p id="textoP">Qué bueno que llegaste :) . </p>
-                            <p id="textoP">¿Estás listx para crear un nuevo tablero o seguir trabajando en un proyecto anterior?</p>
-                        
-                        <div id="imgBoardsBtn" className='container'>
-                            <div className="col-md-6" id="imgFeatures">
-                                <img id="imgLogged" src={Feature_3} className="img-fluid" alt="calendar" style={{ width: '400px' }} />
-                            </div>
-                        
-                            <div className="container-fluid">
-                            <h1>Mis Tableros</h1>
-                            <div id='tablerosUsuario' className='col-mb-6'>
-                                <h2>Tablero1</h2>
-                                <h2>Tablero2</h2>
-                                <h2>Tablero3</h2>
-                            </div>
-
-                            <div id="botonCrearTablero" className='col-mb-6'>
-                                <button id="btnCrearTablero" type="button" className="btn btn-secondary">Nuevo Tablero</button>
-                            </div>
-
-                            </div>
-                            </div>
+            <div className="container-fluid" id="FondoTogged">
+                <div className="tituloLogged text-center">
+                    <h1>¡Hola, {name}!</h1>
+                    <p>Qué bueno que llegaste. </p>
+                    <p>¿Estás listx para crear un nuevo tablero o seguir trabajando en un proyecto anterior?</p>
+                </div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6 img">
+                            <img class="img-fluid" src={Feature_3} alt="" style={{width:"600px"}}/>
+                        </div>
+                        <div className="col-md-5" id="Tableros">
+                            <table className="table text-center">
+                                <thead className="thead-dark">
+                                    <tr>
+                                        <th scope="col">Mis Tableros</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{table[0]}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{table[1]}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{table[2]}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -43,3 +65,12 @@ const Logged = () => {
 }
 
 export default Logged;
+
+{/* <tbody>
+{table.map(
+    <tr>
+        <th>{table.id}</th>
+        <th>{table.nombre}</th>
+    </tr>
+)}
+</tbody> */}
--- ProjectFront/src/components/Login.jsx
+++ ProjectFront/src/components/Login.jsx
@@ -1,69 +1,61 @@
 import React, {useState} from 'react';
+import {useNavigate} from 'react-router';
 import Navbar from './Navbar.jsx';
-import axios from 'axios';
 
 
 
 const Login = () => {
 
-    const [body, setBody] = useState({
-        usuario: '',
-        contraseña: ''
-    });
+    let navigate = useNavigate()
 
-    const inputChange = ({target}) => {
-        const {name, value} = target;
-        setBody({
-            ...body,
-            [name]: value
-        })
-    }
+    const [formData, setFormData] = useState({
+        email: "", 
+        password: ""
+    })
 
-    const onSubmit = () => {
-        console.log(body);
-        axios.post('http://localhost:3000/inicio', body)
-        .then(({data})=> {
-            console.log(data);// mandar a inicio
-        })
-        .catch(({response}) => {
-            console.log(response);// mensaje contraseña y usuario incorrecto.
-        })
+    const iniciarSesion = async (e) => {
+        e.preventDefault()
+        console.log(formData);
+        let {email, password} = formData
+        
+        try {
+            const respuesta = await fetch('http://localhost:4000/user', {
+                method: "POST",
+                body: JSON.stringify({email, password}),
+                headers: {'Content-Type': 'application/json'}
+            })
+    
+            const data = await respuesta.json()
+            
+            if(data.lenght !== 0) {
+                console.log(data[0]);
+                navigate("/Board")
+                
+            }
+            
+        }catch(err) {
+            alert('Usuario o contraseña incorrectos')
+            console.error(err)
+        }
+
     }
 
     return (
         <div>
             <Navbar />
-        
         <div id='login' className='row justify-content-center'>
         <div className="col-md-4 text-center" id="form">
-            <form>
+            <form onSubmit= {iniciarSesion}>
                 <h2 className="text-center">Iniciar sesión</h2>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-                    <input 
-                    name= "usuario" 
-                    onChange= {inputChange} 
-                    value= {body.usuario} 
-                    type= "email"
-                    className= "form-control"
-                    id= "exampleInputEmail1"
-                    aria-describedby= "emailHelp" 
-                    required
-                    />
+                    <input name="email" onChange={(e) => setFormData({...formData, email: e.target.value})} value={formData.email} type="email" className= "form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input
-                    name= "contraseña" 
-                    onChange= {inputChange} 
-                    value= {body.contraseña} 
-                    type= "password"  
-                    className= "form-control" 
-                    id= "exampleInputPassword1" 
-                    required
-                    />
+                    <input name="password" onChange={(e) => setFormData({...formData, password: e.target.value})} value={formData.password} type="password" className="form-control" id="exampleInputPassword1" required/>
                 </div>
-                <button onClick={onSubmit} id='botonLogin' type="submit" className="btn btn-primary">Iniciar sesión</button>
+                <button id='botonLogin' type="submit" className="btn btn-primary">Iniciar sesión</button>
             </form>
         </div>
         </div>
