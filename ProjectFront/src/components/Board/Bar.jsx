dc556ab8170c5579387061990e6fa0dad02f54eb
Paula <91703336+PaulaBecariaC@users.noreply.github.com>
2022-03-30 14:09:44 -0300

    css 30

--- ProjectFront/src/components/Board/Bar.jsx
+++ ProjectFront/src/components/Board/Bar.jsx
@@ -8,7 +8,10 @@ const Bar = () => {
             <div className='col-md-3 boxTablero'>
                 <input type="text" className="form-control m-2" id="nombreTablero" placeholder="Escriba el nombre del tablero"></input>
             </div>
-                <button id="botonInvitar" type="button" className="btn btn-success ms-auto">Invitar +</button>
+                <button id="botonEliminarTablero" type="button" className="btn btn-success">Eliminar Tablero</button>
+                <button id="botonInvitar" type="button" className="btn btn-success">Invitar +</button>
+                
+                
         </div>
 
     );
